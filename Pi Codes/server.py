f3203a50debf33006679e3795e8e06742e4b6902
Vipul Negi <vipulhld001@gmail.com>
2025-10-17 17:46:43 +0530

    Delete Pi Codes directory

--- Pi Codes/server.py
+++
@@ -1,9 +0,0 @@
-import flwr as fl
-
-
-NUM_ROUNDS = 20  # Number of rounds to train
-# Start Flower server
-fl.server.start_server(
-  server_address="192.168.10.103:8080",
-  config=fl.server.ServerConfig(num_rounds=NUM_ROUNDS),
-)
